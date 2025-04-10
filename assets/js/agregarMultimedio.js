c6309ca15234a191951868bf75deb86938685f7e
Rogerfps <rpereira40667@ufide.ac.cr>
2025-04-10 12:46:44 -0600

    CSS: Multime-Cliente-Tarifa

---
+++ assets/js/agregarMultimedio.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector('form').addEventListener('submit', function(e) {
+        const titulo = document.getElementById('titulo').value.trim();
+        if (!titulo) {
+            alert('Por favor ingresa un título para el multimedia');
+            e.preventDefault();
+        }
+
+        
+    });
+});
